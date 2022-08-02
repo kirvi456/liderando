435fd3629dd5ceb76aef0209fc0a5c095339421d
kirvi456 <kirvi16@gmail.com>
2022-08-02 09:44:31 -0600

    Agregado balance total

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const STATIC_CACHE      = 'static-v1';
+const STATIC_CACHE      = 'static-v4';
 const DYNAMIC_CACHE     = 'dynamic-v1';
 const INMUTABLE_CACHE   = 'inmutable-v1';
 
@@ -8,8 +8,9 @@ const APP_SHELL = [
     'favicon.ico',
     'logo192.png',
     'logo512.png',
+
     'static/css/main.3ce23c5e.css',
-    'static/js/main.654708b7.js',
+    'static/js/main.e984b5e8.js',
     'static/media/calculator.5a01297bfefdde7d8a53f1044ede79b6.svg',
     'static/media/building.329c66f0728393baa7918c0ebc80d69a.svg',
     'static/media/logo.4951ee3b293e28867515.png',
